82bbe6b24688ee91bf32598b3a60f5db9bb42d79
YYu HHwan KKim <31001511+Kimyuhwanpeter@users.noreply.github.com>
2021-10-13 13:28:53 +0900

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -4,9 +4,14 @@ import easydict
 import cv2
 import os
 
-FLAGS = easydict.EasyDict({"GT_path": "D:/[1]DB/[5]4th_paper_DB/crop_weed/datasets_IJRR2017/annotations/ijrr_annotations_160523",
+FLAGS = easydict.EasyDict({"GT_path": "D:/[1]DB/[5]4th_paper_DB/crop_weed/datasets_IJRR2017/train_rgb_mask",
                            
-                           "GT_mask_path": "D:/[1]DB/[5]4th_paper_DB/crop_weed/datasets_IJRR2017/annotations/new_annotation"})
+                           "GT_mask_path": "D:/[1]DB/[5]4th_paper_DB/crop_weed/datasets_IJRR2017/train_aug_gray_mask"})
+
+color_map = {(0, 50, 255): 255,
+             (0, 0, 255): 255, 
+             (255, 50, 0): 128,
+             (255, 0, 0): 128}
 
 if __name__ == "__main__":
     total_GT = os.listdir(FLAGS.GT_path)
@@ -14,25 +19,49 @@ if __name__ == "__main__":
 
     for i in range(len(total_GT)):
         img = cv2.imread(total_GT[i])
-        img_copy = np.zeros([img.shape[0], img.shape[1]], dtype=np.uint8)
-        #a = np.where(img == [0, 0, 255], 255, img)
+        img_copy = np.zeros([img.shape[0], img.shape[1]], dtype=np.long)
 
-        for y in range(img.shape[0]):
-            for x in range(img.shape[1]):
-                b = img[y,x][0]
-                g = img[y,x][1]
-                r = img[y,x][2]
+        #for y in range(img.shape[0]):
+        #    for x in range(img.shape[1]):
+        func = lambda b: b[:, :, 0] == 0 
+        func2 = lambda g: g[:, :, 1] == 0 
+        func3 = lambda r: r[:, :, 2] == 255
 
-                if b == 0 and g == 0 and r == 255:
-                    img_copy[y,x] = 255
+        func4 = lambda b: b[:, :, 0] == 0 
+        func5 = lambda g: g[:, :, 1] == 50 
+        func6 = lambda r: r[:, :, 2] == 255
 
-                elif b == 0 and g == 0 and r == 0:
-                    img_copy[y,x] = 0
+        func7 = lambda b: b[:, :, 0] == 255 
+        func8 = lambda g: g[:, :, 1] == 0 
+        func9 = lambda r: r[:, :, 2] == 0
 
-                else:
-                    img_copy[y,x] = 128
-       
-        cv2.imwrite(FLAGS.GT_mask_path + "/" + total_GT[i].split('/')[-1], img_copy)
+        func10= lambda b: b[:, :, 0] == 255 
+        func11 = lambda g: g[:, :, 1] == 50 
+        func12 = lambda r: r[:, :, 2] == 0
+
+        img_copy = np.where(func(img) & func2(img) & func3(img), 255, 0)
+        img_copy = np.where(func4(img) & func5(img) & func6(img), 255, img_copy)
+        img_copy = np.where(func7(img) & func8(img) & func9(img), 128, img_copy)
+        img_copy = np.where(func10(img) & func11(img) & func12(img), 128, img_copy)
+
+        #print(np.bincount(np.reshape(img_copy, [img.shape[0]*img.shape[1]])))
+
+        img_copy2 = cv2.flip(img_copy, -1)
+
+        name = total_GT[i].split('/')[-1].split("_")[3].split("frame")[1]
+        name2 = total_GT[i].split('/')[-1].split("_")[3].split("frame")[1]
+        if len(name) == 2:
+            name = "rgb_00"+ name + ".png"
+            name2 = "rgb_00"+ name2 + "_2" + ".png"
+        elif len(name) == 1:
+            name = "rgb_000"+ name + ".png"
+            name2 = "rgb_000"+ name2 + "_2" + ".png"
+        else:
+            name = "rgb_0"+ name + ".png"
+            name2 = "rgb_0"+ name2 + "_2" + ".png"
+
+        cv2.imwrite(FLAGS.GT_mask_path + "/" + name, img_copy)
+        cv2.imwrite(FLAGS.GT_mask_path + "/" + name2, img_copy2)
 
 
         a = 0
